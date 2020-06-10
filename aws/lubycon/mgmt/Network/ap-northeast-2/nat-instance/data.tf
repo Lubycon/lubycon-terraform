c9300d20b6422d30eaa6420ecb8ab2a947c81449
danielzepp <bboyzepot@gmail.com>
2020-06-11 01:50:55 +0900

    Add nat-instance

---
+++ aws/lubycon/mgmt/Network/ap-northeast-2/nat-instance/data.tf
@@ -0,0 +1,19 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket  = var.remote_backend_bucket
+    key     = var.vpc_remote_backend_bucket_key
+    region  = var.remote_backend_bucket_region
+    profile = var.aws_profile
+  }
+}
+
+data "terraform_remote_state" "sg" {
+  backend = "s3"
+  config = {
+    bucket  = var.remote_backend_bucket
+    key     = var.sg_remote_backend_bucket_key
+    region  = var.remote_backend_bucket_region
+    profile = var.aws_profile
+  }
+}
