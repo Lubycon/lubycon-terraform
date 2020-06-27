fe8e7e002ecd04f7aa8b7f7a76603cd36d4badba
Evan <bboydart91@gmail.com>
2020-06-27 23:42:35 +0900

    feat: 기본적인 에셋 버킷 리소스 설정

---
+++ aws/lubycon/mgmt/Network/ap-northeast-2/assets/vars.tf
@@ -0,0 +1,54 @@
+####################
+# AWS Configure
+####################
+variable "aws_region" {
+}
+
+variable "aws_profile" {
+}
+
+provider "aws" {
+  region = var.aws_region
+  profile = var.aws_profile
+}
+####################
+# Remote Data Source
+####################
+variable "remote_backend_bucket" {
+  description = "remote_backend_bucket"
+  default = "lubycon-devops-mgmt"
+}
+
+variable "remote_backend_bucket_region" {
+  description = "remote_backend_bucket_region"
+  default = "ap-northeast-2"
+}
+variable "vpc_remote_backend_bucket_key" {
+  description = "vpc_remote_backend_bucket_key"
+}
+variable "sg_remote_backend_bucket_key" {
+  description = "sg_remote_backend_bucket_key"
+}
+
+####################
+# CloudFront & S3
+####################
+variable "namespace" {
+  type = string
+  default = "eg"
+}
+
+variable "name" {
+  type = string
+  default = "assets"
+}
+
+variable "stage" {
+  type = string
+  default = "prod"
+}
+
+variable "parent_zone_name" {
+  type = string
+  default = ""
+}
