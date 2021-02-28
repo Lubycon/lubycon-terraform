0ec3020cef24833408836f01d2ba271aaad1344a
danielzepp <bboyzepot@gmail.com>
2021-02-28 09:52:00 +0900

    all github pages use A record

--- aws/lubycon/mgmt/Global/route53/records.tf
+++ aws/lubycon/mgmt/Global/route53/records.tf
@@ -43,15 +43,15 @@ module "alpha-lubycon-io-records" {
   records = [
     {
       name = ""
-      type = "CNAME"
+      type = "A"
       ttl  = 30
-      records = [var.lubycon_io_github_pages_domain]
+      records = var.github_pages_ip_addresses
     },
     {
       name = "ui-kit"
-      type = "CNAME"
+      type = "A"
       ttl  = 30
-      records = [var.lubycon_io_github_pages_domain]
+      records = var.github_pages_ip_addresses
     },
   ]
 }
